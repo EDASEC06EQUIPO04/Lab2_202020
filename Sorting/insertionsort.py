86641151a3aee0c67fc5021fbf421d9954c42913
lauravannesapardo2015 <lauravannesapardo@gmail.com>
2020-08-26 18:31:45 -0500

    App actualizado

--- Sorting/insertionsort.py
+++ Sorting/insertionsort.py
@@ -29,40 +29,69 @@ def lessfunction (elemento1, elemento2):
     return False
 
 
-def insertionSort (lst, orden:str)->list: 
+def insertionSort (lst, criterio:str, orden:str)->list: 
    
-
+  #criterio='vote_count'
+  criterio=criterio
+      
+  if criterio=='vote_count':
+    
     size =  len(lst)-1 
     pos1 = 1
     
-    #for i  in range (0,50,):
-    #  print (lst[i])
-    #input ("clic")
-
     for index in range(1,len(lst)):
-     currentvalue = int(lst[index]['vote_count'])
-     position = index
-     original=lst[index]
-     #print (currentvalue, " " , index , "  ", position)
-     #input ("clic para avanzaar")
-     while position>0 and int(lst [position-1]['vote_count'])>int(currentvalue):
-         lst [position]=lst [position-1]
-         position = position-1
-         #print (currentvalue)
-     lst[position]=original
+      currentvalue = int(lst[index][criterio])
+      position = index
+      original=lst[index]
+      #print (currentvalue, " " , index , "  ", position)
+      #input ("clic para avanzaar")
+      while position>0 and int(lst [position-1][criterio])>int(currentvalue):
+          lst [position]=lst [position-1]
+          position = position-1
+          #print (currentvalue)
+      lst[position]=original
+
+   
+
+    if (orden == "less"):
+            for i  in range (0,len(lst),1):
+                print (lst[i][criterio])
+            print ("Se ordeno ascendentemente")
+            input ("Se finalizo el proceso...")
+    if (orden=="greater"):
+            input ("estoy aqui")
+            for i  in range (1,len(lst),1):
+              print (lst[len(lst)-i][criterio])
+            print ("Se ordeno Descendentemente")
+            input ("Se finalizo el proceso...")
+
+  if criterio=='vote_average':
+   
+    size =  len(lst)-1 
+    pos1 = 1
+    
+    for indexe in range(1,len(lst)):
+          print (lst[indexe][criterio])    
+          currentvalue = (lst[indexe][criterio])
+          position = indexe
+          original=lst[indexe]
+          while (position>0 and (lst [position-1][criterio])>(currentvalue)):
+              lst [position]=lst [position-1]
+              position = position-1
+              #print (currentvalue)
+          lst[position]=original
 
     input ("Dar clic para imprimir lista ordenada  en Insertion procedure")
 
     if (orden == "less"):
-          for i  in range (0,len(lst),1):
-              print (lst[i]['vote_count'])
-          print ("Se ordeno ascendentemente")
-          input ("Se finalizo el proceso...")
+            for i  in range (0,len(lst),1):
+                print (lst[i][criterio])
+            print ("Se ordeno ascendentemente")
+            input ("Se finalizo el proceso...")
     if (orden=="greater"):
-          input ("estoy aqui")
-          for i  in range (len(lst),0,1):
-              print (lst[i]['vote_count'])
-          print ("Se ordeno Descendentemente")
-          input ("Se finalizo el proceso...")
-
-    return lst
+            input ("estoy aqui")
+            for i  in range (1,len(lst),1):
+              print (int(lst[len(lst)-i][criterio])*10)
+            print ("Se ordeno Descendentemente")
+            input ("Se finalizo el proceso...")
+  return lst
--- Sorting/shellsort.py
+++ Sorting/shellsort.py
@@ -1,37 +1,11 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación, Universidad de Los Andes
- * 
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
-
-
 import config as cf
 from ADT import list as lt
+#from Sorting.insertionsort import lessfunction
 
-"""
-Implementación del algoritmo shellsort, basado en la propuesta de Robert Sedgewick
-
-Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne
-
-Se utiliza la secuencia de incrementos 3x+1: 1, 4, 13, 40, 121, 364, 1093, ..... (D. Knuth)
-Sedgewick: 1,5,19,41,109,209,929,2161,...
-"""
+def lessfunction (elemento1, elemento2):
+    if elemento1<elemento2:
+      return True
+    return False
 
 def shellSort(lst, lessfunction):
     n = lt.size(lst)
@@ -45,3 +19,7 @@ def shellSort(lst, lessfunction):
                 lt.exchange (lst, j+1, j-h+1)
                 j -=h
         h //=3              # h se decrementa en un tercio. cuando h es igual a 1, se comporta como insertionsort
+
+lst=[]
+lst=[1,6,8,2,3,90,7,6,7,44,56]  
+shellSort(lt)
